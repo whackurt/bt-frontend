425429e0265c4685d7e89e33b55caf92c730ca02
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2023-11-12 02:17:19 +0800

    refactor: changed qr code scanner due to version incompatibility

--- lib/features/establishment_features/scan_qr/presentation/screens/scan_qr_code.dart
+++ lib/features/establishment_features/scan_qr/presentation/screens/scan_qr_code.dart
@@ -1,124 +1,110 @@
-import 'dart:io';
 import 'package:bt_frontend/widgets/appbar.dart';
+import 'package:bt_frontend/widgets/custom_buttons/full_width_btn.dart';
 import 'package:flutter/material.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class BTScanQR extends StatefulWidget {
   const BTScanQR({super.key});
+
   @override
   State<BTScanQR> createState() => _BTScanQRState();
 }
 
 class _BTScanQRState extends State<BTScanQR> {
-  final qrKey = GlobalKey(debugLabel: 'QR');
-  QRViewController? controller;
-  Barcode? barcode;
+  String _scanBarcode = 'No Scanned QR Code';
 
-  @override
-  void dispose() {
-    controller?.dispose();
-    super.dispose();
-  }
+  Future<void> scanQR() async {
+    String barcodeScanRes;
 
-  @override
-  void reassemble() async {
-    super.reassemble();
-    if (Platform.isAndroid) {
-      await controller!.pauseCamera();
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancel', true, ScanMode.QR);
+      // ignore: avoid_print
+      print(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
     }
-    controller!.resumeCamera();
+
+    if (!mounted) return;
+
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
   }
 
   @override
-  Widget build(BuildContext context) => SafeArea(
-          child: Scaffold(
-        appBar: appBar(title: 'Scan QR Code'),
-        body: Stack(
-          alignment: Alignment.center,
-          children: [
-            buildQrView(context),
-            Positioned(bottom: 10, child: buildResult()),
-            Positioned(top: 10, child: buildControlButtons())
-          ],
-        ),
-      ));
-
-  Widget buildControlButtons() => Container(
-        padding: const EdgeInsets.all(2.0),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8.0), color: Colors.white),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            IconButton(
-                icon: FutureBuilder(
-                    future: controller?.getFlashStatus(),
-                    builder: ((context, snapshot) {
-                      if (snapshot.data != null) {
-                        return Icon(
-                            snapshot.data! ? Icons.flash_on : Icons.flash_off);
-                      } else {
-                        return Container();
-                      }
-                    })),
-                onPressed: () async {
-                  await controller?.toggleFlash();
-                  setState(() {});
-                }),
-            IconButton(
-                icon: FutureBuilder(
-                    future: controller?.getCameraInfo(),
-                    builder: (context, snapshot) {
-                      if (snapshot.data != null) {
-                        return const Icon(Icons.switch_camera);
-                      } else {
-                        return Container();
-                      }
-                    }),
-                onPressed: () async {
-                  await controller?.flipCamera();
-                  setState(() {});
-                })
-          ],
-        ),
-      );
-
-  Widget buildResult() => Padding(
-        padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 20.0),
-        child: Container(
-          padding: const EdgeInsets.all(12.0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            color: barcode != null ? Colors.white : Colors.white24,
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 247, 245, 245),
+      appBar: appBar(title: 'Scan QR Code'),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              const SizedBox(
+                height: 30.0,
+              ),
+              const CircleAvatar(
+                radius: 75,
+                backgroundImage: NetworkImage(
+                    'https://imageio.forbes.com/specials-images/imageserve/614898161330562654811729/BVLGARI--Serpenti-Seduttori--Launch--Photocall/0x0.jpg?format=jpg&crop=2398,1599,x0,y92,safe&width=960'),
+                backgroundColor: Colors.white,
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+              Text('Lalisa Manobal',
+                  maxLines: 2,
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.grey[800])),
+              const SizedBox(
+                height: 30.0,
+              ),
+              Container(
+                padding: const EdgeInsets.all(20.0),
+                height: 270.0,
+                decoration: BoxDecoration(color: Colors.grey[200]),
+                child: Column(
+                  children: [
+                    Text(_scanBarcode,
+                        style: const TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.indigo)),
+                    _scanBarcode == 'No Scanned QR Code'
+                        ? const Text('')
+                        : QrImageView(
+                            data: _scanBarcode,
+                            version: QrVersions.auto,
+                            size: 200,
+                            errorStateBuilder: (ctx, err) {
+                              return const Center(
+                                child: Text(
+                                  'Something went wrong!!!',
+                                  textAlign: TextAlign.center,
+                                ),
+                              );
+                            },
+                          ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 100.0,
+              ),
+              BTFullWidthButton(
+                  onPressed: () => scanQR(), label: 'Scan QR Code'),
+            ],
           ),
-          child: Text(
-            barcode != null
-                ? 'Result: ${barcode!.code}'
-                : 'Place a code in the frame',
-            maxLines: 3,
-          ),
         ),
-      );
-
-  Widget buildQrView(BuildContext context) => QRView(
-        key: qrKey,
-        onQRViewCreated: onQRViewCreated,
-        overlay: QrScannerOverlayShape(
-          borderColor: barcode != null
-              ? const Color.fromARGB(255, 40, 48, 168)
-              : const Color.fromARGB(255, 200, 100, 100),
-          borderRadius: 15.0,
-          borderLength: 20.0,
-          borderWidth: 10.0,
-          cutOutSize: MediaQuery.of(context).size.width * 0.7,
-        ),
-      );
-
-  void onQRViewCreated(QRViewController controller) {
-    setState(() => this.controller = controller);
-    controller.scannedDataStream
-        .listen((barcode) => setState(() => this.barcode = barcode));
+      ),
+    );
   }
 }
