12dbdc8b607739dc7b187bbb9bfd6869bddee010
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2023-11-22 17:39:05 +0800

    feat: implemented tourist home

--- lib/config/api.dart
+++ lib/config/api.dart
@@ -1,4 +1,10 @@
 class Api {
   String baseUrl = "http://192.168.254.105:8000/api";
+
+  String touristBaseUrl = "http://192.168.254.105:8000/api/v1/tourist";
+
+  String establishmentBaseUrl =
+      "http://192.168.254.105:8000/api/v1/establishment";
+
   String cloudinaryUrl = "https://api.cloudinary.com/v1_1/dyi2sla5u/upload";
 }
