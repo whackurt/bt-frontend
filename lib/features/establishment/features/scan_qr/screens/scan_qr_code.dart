7e757c9e6caa32996186c90a53cd826cf2af715d
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2023-12-21 22:20:09 +0800

    feat: scan qr code

--- lib/features/establishment/features/scan_qr/screens/scan_qr_code.dart
+++ lib/features/establishment/features/scan_qr/screens/scan_qr_code.dart
@@ -1,9 +1,16 @@
-import 'package:bt_frontend/widgets/appbar.dart';
+import 'package:bt_frontend/features/establishment/features/entry_logs/controllers/log.controller.dart';
+import 'package:bt_frontend/features/establishment/features/entry_logs/models/log.model.dart';
+import 'package:bt_frontend/features/establishment/providers/entry_logs.provider.dart';
 import 'package:bt_frontend/widgets/custom_buttons/full_width_btn.dart';
+import 'package:bt_frontend/widgets/custom_text/app_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class BTScanQR extends StatefulWidget {
   const BTScanQR({super.key});
@@ -13,12 +20,18 @@ class BTScanQR extends StatefulWidget {
 }
 
 class _BTScanQRState extends State<BTScanQR> {
-  String _scanBarcode = 'No Scanned QR Code';
+  EntryLogsController entryLogsController = EntryLogsController();
+
+  String qrCodeResult = 'No Scanned QR Code';
+  List<dynamic>? logDetails = [];
+  bool loading = false;
+  bool invalidQrCode = false;
 
   Future<void> scanQR() async {
     String barcodeScanRes;
 
     try {
+      logDetails = [];
       barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
           '#ff6666', 'Cancel', true, ScanMode.QR);
       // ignore: avoid_print
@@ -30,7 +43,44 @@ class _BTScanQRState extends State<BTScanQR> {
     if (!mounted) return;
 
     setState(() {
-      _scanBarcode = barcodeScanRes;
+      qrCodeResult = barcodeScanRes;
+    });
+
+    if (qrCodeResult != '-1') {
+      await createLog();
+    }
+  }
+
+  Future createLog() async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    setState(() {
+      loading = true;
+      invalidQrCode = false;
+      logDetails = [];
+    });
+    await entryLogsController
+        .createLog(
+            log: Log(
+                qrCode: qrCodeResult,
+                dateTime: DateTime.now(),
+                establishmentId: pref.getInt('establishmentId')))
+        .then((res) {
+      if (res['success']) {
+        setState(() {
+          logDetails = res['data']['log'];
+        });
+      } else {
+        setState(() {
+          invalidQrCode = true;
+        });
+      }
+    });
+    setState(() {
+      loading = false;
+    });
+
+    await entryLogsController.getEntryLogsToday().then((res) {
+      context.read<EntryLogsProvider>().setEntryLogs(data: res['data']['logs']);
     });
   }
 
@@ -38,81 +88,135 @@ class _BTScanQRState extends State<BTScanQR> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: appBar(title: 'Scan QR Code'),
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              const SizedBox(
-                height: 30.0,
-              ),
-              const CircleAvatar(
-                radius: 75,
-                backgroundImage: NetworkImage(
-                    'https://imageio.forbes.com/specials-images/imageserve/614898161330562654811729/BVLGARI--Serpenti-Seduttori--Launch--Photocall/0x0.jpg?format=jpg&crop=2398,1599,x0,y92,safe&width=960'),
-                backgroundColor: Colors.white,
-              ),
-              const SizedBox(
-                height: 30.0,
-              ),
-              Text('Lalisa Manobal',
-                  maxLines: 2,
-                  style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.grey[800])),
-              const SizedBox(
-                height: 30.0,
-              ),
-              Container(
-                padding: const EdgeInsets.all(20.0),
-                height: 270.0,
-                decoration: BoxDecoration(color: Colors.grey[200]),
-                child: Column(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Row(
                   children: [
-                    Text(_scanBarcode,
-                        style: const TextStyle(
-                            fontSize: 25,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.indigo)),
-                    _scanBarcode == 'No Scanned QR Code'
-                        ? const Text('')
-                        : QrImageView(
-                            data: _scanBarcode,
-                            version: QrVersions.auto,
-                            size: 200,
-                            errorStateBuilder: (ctx, err) {
-                              return const Center(
-                                child: Text(
-                                  'Something went wrong!!!',
-                                  textAlign: TextAlign.center,
-                                ),
-                              );
-                            },
-                          ),
+                    FaIcon(
+                      FontAwesomeIcons.qrcode,
+                      size: 30.0,
+                      color: Colors.red[400],
+                    ),
+                    const SizedBox(
+                      width: 8.0,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        AppText().purpleBoldHeader(text: 'Scan QR Code'),
+                        Text(
+                          'Record Tourist Entry',
+                          style: TextStyle(color: Colors.grey[700]),
+                        )
+                      ],
+                    ),
                   ],
                 ),
-              ),
-              const SizedBox(
-                height: 40.0,
-              ),
-              BTFullWidthButton(
-                onPressed: () => scanQR(),
-                child: const Text(
-                  'Scan QR Code',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 20.0),
+                const Divider(),
+                const SizedBox(
+                  height: 20.0,
                 ),
-              ),
-              const SizedBox(
-                height: 60.0,
-              ),
-            ],
+                logDetails!.isNotEmpty
+                    ? CircleAvatar(
+                        radius: 75,
+                        backgroundImage: NetworkImage(
+                            '${logDetails![0]['tourist']['photo_url']}'),
+                        backgroundColor: Colors.white,
+                      )
+                    : const SizedBox(
+                        height: 150.0,
+                      ),
+                const SizedBox(
+                  height: 30.0,
+                ),
+                logDetails!.isNotEmpty
+                    ? Text(
+                        '${logDetails![0]['tourist']['first_name']} ${logDetails![0]['tourist']['last_name']}',
+                        maxLines: 2,
+                        style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.grey[800]))
+                    : const SizedBox(
+                        height: 30.0,
+                      ),
+                const SizedBox(
+                  height: 30.0,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(20.0),
+                  height: 270.0,
+                  decoration: BoxDecoration(color: Colors.grey[200]),
+                  child: loading
+                      ? const SpinKitRing(
+                          color: Colors.indigo,
+                          lineWidth: 3.0,
+                        )
+                      : Column(
+                          children: [
+                            invalidQrCode
+                                ? Text('Invalid QR Code',
+                                    style: TextStyle(
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.w600,
+                                        color: Colors.red[400]))
+                                : Text(
+                                    qrCodeResult == '-1'
+                                        ? 'No QR Code Scanned'
+                                        : qrCodeResult,
+                                    style: const TextStyle(
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.w600,
+                                        color: Colors.indigo)),
+                            qrCodeResult == 'No Scanned QR Code' ||
+                                    qrCodeResult == '-1'
+                                ? const SizedBox(
+                                    height: 200.0,
+                                    width: 180.0,
+                                  )
+                                : QrImageView(
+                                    data: qrCodeResult,
+                                    version: QrVersions.auto,
+                                    size: 200,
+                                    errorStateBuilder: (ctx, err) {
+                                      return const Center(
+                                        child: Text(
+                                          'Something went wrong!!!',
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      );
+                                    },
+                                  ),
+                          ],
+                        ),
+                ),
+                const SizedBox(
+                  height: 40.0,
+                ),
+                BTFullWidthButton(
+                  onPressed: () => scanQR(),
+                  child: const Text(
+                    'Scan QR Code',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20.0),
+                  ),
+                ),
+                const SizedBox(
+                  height: 60.0,
+                ),
+              ],
+            ),
           ),
         ),
       ),
