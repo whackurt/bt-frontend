bd0790751c20e743a53c815da6a5a9a0f743aa2b
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2023-12-23 00:51:07 +0800

    refactor: updated app icon-group

--- lib/core/constants/screens/loading_screen.dart
+++ lib/core/constants/screens/loading_screen.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables
 
-import 'package:bt_frontend/core/constants/decoration/app_data.dart';
 import 'package:bt_frontend/widgets/wrapper/init_wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
@@ -34,13 +33,13 @@ class _LoadingState extends State<Loading> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Image(
-                  width: 100.0,
-                  height: 100.0,
-                  image: AssetImage('assets/images/app-icon.png')),
-              AppData().appNameLight(),
-              SpinKitThreeBounce(
+                  width: 270.0,
+                  height: 270.0,
+                  image: AssetImage('assets/images/icon-group.png')),
+              SpinKitRing(
                 color: Colors.white,
-                size: 20.0,
+                size: 40.0,
+                lineWidth: 3,
               ),
             ],
           ),
